31aae760ac36c58177b451d4c190f3f61676fa91
NicolasDeGheselle <apple@3magroup.com>
2026-06-19 16:59:37 +0200

    Fix resources namespace

--- Joufflu.Shared/Navigation/ILayout.cs
+++ Joufflu.Shared/Navigation/ILayout.cs
@@ -5,17 +5,10 @@
     /// </summary>
     public interface IPage
     {
+        public void OnShow()
+        { }
         public void OnHide()
         { }
-    }
-
-    /// <summary>
-    /// Page with a layout as a parent, allow the page to set data in the layout
-    /// </summary>
-    /// <typeparam name="TLayout"></typeparam>
-    public interface IPage<TLayout> : IPage where TLayout : ILayout
-    {
-        public TLayout? ParentLayout { get; set; }
     }
 
     /// <summary>
@@ -23,14 +16,10 @@
     /// </summary>
     public interface ILayout : IPage
     {
+        void Hide(IPage page);
         void Hide();
-        void Show(IPage page);
-    }
 
-    /// <summary>
-    /// Nested layout
-    /// </summary>
-    /// <typeparam name="TLayout"></typeparam>
-    public interface ILayout<TLayout> : ILayout, IPage<TLayout> where TLayout : ILayout
-    {}
+        void Show(IPage page);
+        void Overlay();
+    }
 }
